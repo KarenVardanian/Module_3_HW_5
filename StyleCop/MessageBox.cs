6790c3aa87e72b7672ed4f31aa6603b4ce2fae10
KarenVardanian <karen.vardanian@nure.ua>
2022-01-02 21:46:46 +0200

    Добавьте файлы проекта.

---
+++ StyleCop/MessageBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StyleCop
+{
+    public class MessageBox
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<Ожидание>")]
+        public Action ActionClose;
+        public State St;
+#pragma warning disable CS1998 // В асинхронном методе отсутствуют операторы await, будет выполнен синхронный метод
+        public async void Open()
+#pragma warning restore CS1998 // В асинхронном методе отсутствуют операторы await, будет выполнен синхронный метод
+        {
+            Console.WriteLine("Окно открыто");
+            Task.Delay(300).Wait();
+            Console.WriteLine("Окно закрыто");
+
+            var rn = new Random();
+            var res = rn.Next(1, 3);
+
+            if (res == 1)
+            {
+                St = State.Ok;
+                ActionClose += Open;
+
+                ActionClose?.Invoke();
+            }
+            else
+            {
+                St = State.Cancel;
+                ActionClose += Open;
+
+                ActionClose?.Invoke();
+            }
+        }
+    }
+}
---
+++ StyleCop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StyleCop
+{
+    public enum State
+    {
+        Ok,
+        Cancel
+    }
+
+    public class Program
+    {
+        private static void Main()
+        {
+            var message = new MessageBox();
+            var tcs = new TaskCompletionSource<State>();
+
+            message.ActionClose = message.Open;
+            if (message.St == State.Ok)
+            {
+                Console.Write("операция была подтверждена");
+                tcs.SetResult(State.Ok);
+            }
+            else if (message.St == State.Cancel)
+            {
+                Console.Write("операция была отклонена");
+                tcs.SetResult(State.Cancel);
+            }
+
+            tcs.Task.GetAwaiter().GetResult();
+        }
+    }
+}
